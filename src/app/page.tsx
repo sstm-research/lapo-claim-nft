2caa2d07f19ade6f50843d886a2069b44fa68f0a
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-07-20 23:28:47 -0300

    feat: update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import ClaimNFT from "@/components/ClaimNFT";
+import { PROJECT_URL } from "@/utils/constants";
+import { formatEthAddress } from "@/utils/eth";
 import { createClient } from "@/utils/supabase/server";
+import QRCode from "react-qr-code";
 
 export default async function Home() {
   const supabase = createClient();
 
-  const { data: nfts } = await supabase.from("NFTs").select().order("id");
-  console.log(nfts);
+  const { data: nfts } = await supabase.from("NFTs").select("*").order("id");
 
   return (
-    <main className="mx-auto w-full h-screen flex flex-col gap-4 justify-center items-center">
-      <ClaimNFT />
+    <main className="mx-auto w-full h-screen flex flex-col lg:flex-row gap-8 lg:gap-12 justify-center items-center">
+      {nfts?.map((nft) => (
+        <div key={nft.id} className="font-mono flex flex-col gap-1 text-center">
+          <QRCode
+            size={128}
+            bgColor="black"
+            fgColor="#B1FD00"
+            value={`${PROJECT_URL}/nft/${nft.uuid}`}
+          />
+          {nft.owner ? <p>{formatEthAddress(nft.owner)}</p> : <p>Claimable</p>}
+        </div>
+      ))}
     </main>
   );
 }
--- src/utils/eth.ts
+++ src/utils/eth.ts
@@ -1,5 +1,5 @@
 import { Address } from "thirdweb";
 
 export const formatEthAddress = (address: string | Address) => {
-  return address.slice(0, 6) + "..." + address.slice(-4);
+  return address.slice(0, 4) + "..." + address.slice(-4);
 };
