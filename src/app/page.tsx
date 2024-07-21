5b03c3d51c84f78b5956102d7fcbd23b6ce729db
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-07-20 23:33:30 -0300

    style: add link to qr code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { PROJECT_URL } from "@/utils/constants";
 import { formatEthAddress } from "@/utils/eth";
 import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
 import QRCode from "react-qr-code";
 
 export default async function Home() {
@@ -12,12 +13,14 @@ export default async function Home() {
     <main className="mx-auto w-full h-screen flex flex-col lg:flex-row gap-8 lg:gap-12 justify-center items-center">
       {nfts?.map((nft) => (
         <div key={nft.id} className="font-mono flex flex-col gap-1 text-center">
-          <QRCode
-            size={128}
-            bgColor="black"
-            fgColor="#B1FD00"
-            value={`${PROJECT_URL}/nft/${nft.uuid}`}
-          />
+          <Link href={`${PROJECT_URL}/nft/${nft.uuid}`}>
+            <QRCode
+              size={128}
+              bgColor="black"
+              fgColor="#B1FD00"
+              value={`${PROJECT_URL}/nft/${nft.uuid}`}
+            />
+          </Link>
           {nft.owner ? <p>{formatEthAddress(nft.owner)}</p> : <p>Claimable</p>}
         </div>
       ))}
