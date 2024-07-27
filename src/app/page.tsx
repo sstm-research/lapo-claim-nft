6e8b42120bedcc293383d52a3364007d7a56b4c2
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-07-27 09:46:20 -0300

    style: update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,18 @@
-import { PROJECT_URL } from "@/utils/constants";
-import { formatEthAddress } from "@/utils/eth";
-import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
 import Link from "next/link";
-import QRCode from "react-qr-code";
 
 export default async function Home() {
-  const supabase = createClient();
-
-  const { data: nfts } = await supabase.from("NFTs").select("*").order("id");
-
   return (
     <main className="mx-auto w-full h-screen flex flex-col lg:flex-row gap-8 lg:gap-12 justify-center items-center">
-      {nfts?.map((nft) => (
-        <div key={nft.id} className="font-mono flex flex-col gap-1 text-center">
-          <Link href={`/nft/${nft.uuid}`}>
-            <QRCode
-              size={128}
-              bgColor="black"
-              fgColor="#B1FD00"
-              value={`${PROJECT_URL}/nft/${nft.uuid}`}
-            />
-          </Link>
-          {nft.owner ? <p>{formatEthAddress(nft.owner)}</p> : <p>Claimable</p>}
-        </div>
-      ))}
+      <Link href="https://laposports.com/categoria-produto/major-nature-league/">
+        <Image
+          src="/logo.png"
+          alt="Lapô logo"
+          width={200}
+          height={200}
+          className="pointer-events-none"
+        />
+      </Link>
     </main>
   );
 }
